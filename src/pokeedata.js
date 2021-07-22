60e26ffb48c568f714d95bb547443845fd6b1461
stefysoet <stefysoet@hotmail.com>
2021-07-22 10:08:56 -0500

    actuliación

--- src/pokeedata.js
+++
@@ -1,8 +0,0 @@
-//funcion filter
-export function filtrar(tipoSelec, pokeInfo) {
-  return pokeInfo.filter((poke) => poke.type.includes(tipoSelec));
-}
-export function verRegion(region, pokeInfo) {
-  const datoP = pokeInfo.filter((pokemon) => pokemon.generation.name == region);
-  return datoP;
-}
